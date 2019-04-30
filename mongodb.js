20b93036e89e7510a345fedaf40bd292c607a0d3
Arthur Lam <lam.arthur0311@gmail.com>
2019-04-30 22:42:21 +0000

    init commit

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://localhost:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+      name: 'Arthur',
+      age: 28
+    });
+  }
+);
