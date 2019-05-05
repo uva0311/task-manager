1082151b2aa7a3be15dc61818d1a379cf5a1b0c5
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-05 17:37:57 +0000

    imported User model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+// connect to mongoose
+require('./db/mongoose');
+
+const User = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.post('/users', (req, res) => {
-  res.send('testing');
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch(() => {});
 });
 
 app.listen(port, () => {
