e1d202961908bc88c32a15134fe5bae90e5c8f27
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 00:42:59 +0000

    created tasks endpoint

--- src/index.js
+++ src/index.js
@@ -3,23 +3,38 @@ const express = require('express');
 require('./db/mongoose');
 
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// users endpoint
 app.post('/users', (req, res) => {
   const user = new User(req.body);
 
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch(e => {
-      res.status(400);
-      res.send(e);
+      res.status(400).send(e);
+    });
+});
+
+// tasks endpoint
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch(e => {
+      res.status(400).send(e);
     });
 });
 
