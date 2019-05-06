95050bd455a7b97e8fc9ad23e9c263ee0e3cf12b
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 19:13:31 +0000

    created user data update endpoint with update vaildation

--- src/index.js
+++ src/index.js
@@ -49,6 +49,33 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+// update existing user data
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every(update => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 // tasks endpoint
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
