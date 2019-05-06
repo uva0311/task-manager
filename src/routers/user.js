fdc3614120455434afc1ace9b64e64b53403eb78
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 23:51:21 +0000

    created user login endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,19 @@ router.post('/users', async (req, res) => {
   }
 });
 
+// login users
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
 // fetching all user data from database
 router.get('/users', async (req, res) => {
   try {
