938f58b865c31fcab2cf588c1866b045c8f72248
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 19:38:33 +0000

    created delete task endpoint

--- src/index.js
+++ src/index.js
@@ -159,6 +159,20 @@ app.patch('/tasks/:id', async (req, res) => {
   }
 });
 
+// delete task endpoint
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
