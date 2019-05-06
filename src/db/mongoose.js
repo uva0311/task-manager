4a11f647e002067bb8c41ce3f02281f96af9c18c
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 02:07:41 +0000

    include option to address deprecation warning

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify: false
 });
