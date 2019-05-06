05040eb4fcaa9835c105ffe62c74c1a25d9079a5
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 20:42:32 +0000

    fix bug

--- src/routers/task.js
+++ src/routers/task.js
@@ -54,11 +54,13 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id);
+    const task = await Task.findById(req.params.id);
 
     updates.forEach(update => {
       user[update] = req.body[update];
     });
+
+    await task.save();
 
     if (!task) {
       return res.status(404).send();
