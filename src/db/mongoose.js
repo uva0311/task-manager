10beddc34ddcec6e8e13e3ffc8fa0b2a6e21b5eb
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 00:42:02 +0000

    remove Task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,15 +4,3 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-});
