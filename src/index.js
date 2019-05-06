3251a0d3120c0e6c3d78a82fdd45a627aeccf4a3
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 19:58:59 +0000

    removed all task endpoints to ./routers/task.js

--- src/index.js
+++ src/index.js
@@ -5,93 +5,14 @@ require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
 const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
-
-// tasks endpoint
-app.post('/tasks', async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-// fetching all task data from database
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-// fetching task data from database by id
-app.get('/tasks/:id', async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-
-    if (!task) {
-      return res.status(404).send();
-    }
-
-    res.send(task);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-// update existing task data
-app.patch('/tasks/:id', async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['description', 'completed'];
-  const isValidOperation = updates.every(update => {
-    return allowedUpdates.includes(update);
-  });
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates' });
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!task) {
-      return res.status(404).send();
-    }
-
-    res.send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-// delete task endpoint
-app.delete('/tasks/:id', async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) {
-      return res.status(404).send();
-    }
-
-    res.send(task);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
