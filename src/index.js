9f53b1945ea9556b225dd05e536610221636aba5
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-06 17:25:11 +0000

    refractor endpoints with async-await syntax

--- src/index.js
+++ src/index.js
@@ -11,85 +11,81 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // users endpoint
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // fetching all user data from database
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => {
-      res.send(users);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // fetching user data from database by id
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send();
-      }
 
-      res.send(user);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // tasks endpoint
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // fetching all task data from database
-app.get('/tasks', (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.send(tasks);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // fetching task data from database by id
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
-  Task.findById(_id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send();
-      }
 
-      res.send(task);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+  try {
+    const task = await Task.findById(_id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
