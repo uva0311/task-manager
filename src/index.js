489b80e55455c44d0dff902aa8d25962893be46f
Arthur Lam <lam.arthur0311@gmail.com>
2019-05-07 19:59:43 +0000

    placed experimental code

--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  if (req.method === 'GET') {
-    res.send('GET requests are disabled');
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled');
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send('Site is under maintenance, please come back later');
+// });
 
 app.use(express.json());
 app.use(userRouter);
@@ -25,3 +29,15 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
+
+const main = async () => {
+  // const task = await Task.findById('5cd1df9417e3ff3eab2b29d3');
+  // await task.populate('owner').execPopulate();
+  // console.log(task.owner);
+
+  const user = await User.findById('5cd1dd858480333d9a9ce386');
+  await user.populate('tasks').execPopulate();
+  console.log(user.tasks);
+};
+
+main();
